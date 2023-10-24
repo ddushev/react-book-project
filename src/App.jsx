4b80f312910a918ed24539704e27acc9e70e9d64
Daniel Dushev <danieldyshew@gmail.com>
2023-10-24 12:02:58 +0300

    Add Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
+import { Footer } from "./components/Footer/Footer"
+import { Header } from "./components/Header/Header"
+import { BackToTop } from "./components/Common/BackToTop"
+import { Spinner } from "./components/Common/Spinner"
+import { Carousel } from "./components/Home/Carousel/Carousel"
+import { Booking } from "./components/Home/Booking/Booking"
 
 function App() {
 
   return (
     <div className="container-xxl bg-white p-0">
+      <Header />
+      <Carousel />
+      <Booking />
+      <Footer />
+      <BackToTop />
     </div>
   )
 }
