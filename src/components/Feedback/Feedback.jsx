34157e0e9d911e0df53b56b91b5f1d3ec52f6648
Daniel Dushev <danieldyshew@gmail.com>
2023-10-25 18:11:11 +0300

    Removed unneeded elements

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -11,48 +11,6 @@ export const Feedback = () => {
                     </h1>
                 </div>
                 <div className="row g-4">
-                    <div className="col-12">
-                        {/* <div className="row gy-4">
-                            <div className="col-md-4">
-                                <h6 className="section-title text-start text-primary text-uppercase">
-                                    Booking
-                                </h6>
-                                <p>
-                                    <i className="fa fa-envelope-open text-primary me-2" />
-                                    danieldyshew@example.com
-                                </p>
-                            </div>
-                            <div className="col-md-4">
-                                <h6 className="section-title text-start text-primary text-uppercase">
-                                    General
-                                </h6>
-                                <p>
-                                    <i className="fa fa-envelope-open text-primary me-2" />
-                                    info@example.com
-                                </p>
-                            </div>
-                            <div className="col-md-4">
-                                <h6 className="section-title text-start text-primary text-uppercase">
-                                    Technical
-                                </h6>
-                                <p>
-                                    <i className="fa fa-envelope-open text-primary me-2" />
-                                    tech@example.com
-                                </p>
-                            </div>
-                        </div> */}
-                    </div>
-                    <div className="col-md-6 wow fadeIn" data-wow-delay="0.1s">
-                        <iframe
-                            className="position-relative rounded w-100 h-100"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                            frameBorder={0}
-                            style={{ minHeight: 350, border: 0 }}
-                            allowFullScreen=""
-                            aria-hidden="false"
-                            tabIndex={0}
-                        />
-                    </div>
                     <div className="col-md-6">
                         <div className="wow fadeInUp" data-wow-delay="0.2s">
                             <form>
@@ -110,6 +68,17 @@ export const Feedback = () => {
                                 </div>
                             </form>
                         </div>
+                    </div>
+                    <div className="col-md-6 wow fadeIn" data-wow-delay="0.1s">
+                        <iframe
+                            className="position-relative rounded w-100 h-100"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                            frameBorder={0}
+                            style={{ minHeight: 350, border: 0 }}
+                            allowFullScreen=""
+                            aria-hidden="false"
+                            tabIndex={0}
+                        />
                     </div>
                 </div>
             </div>
