2be631cccf2f14b7ee52c574349b5fc8aa4f18c3
Daniel Dushev <danieldyshew@gmail.com>
2023-10-29 17:14:31 +0200

    Add parking select

--- src/components/AddRoom/AddRoom.jsx
+++ src/components/AddRoom/AddRoom.jsx
@@ -13,6 +13,7 @@ export const AddRoom = () => {
         bed: '1',
         bath: '1',
         wifi: 'Yes',
+        parking: 'Yes',
         description: ''
     }, onAddRoomSubmit);
     return (
@@ -196,37 +197,20 @@ export const AddRoom = () => {
                                         </div>
                                         <div className="col-6">
                                             <div className="form-floating" >
-                                                {/* <select name="wifi" value={values.wifi} onChange={onChangeHandler} className="form-select" id="select5">
+                                                <select name="wifi" value={values.wifi} onChange={onChangeHandler} className="form-select" id="select5">
                                                     <option>Yes</option>
                                                     <option>No</option>
                                                 </select>
-                                                <label htmlFor="select5">WiFi</label> */}
-                                                <div>
-                                                    <div className="radio">
-                                                        <label>
-                                                            <input
-                                                                type="radio"
-                                                                name="wifi"
-                                                                onChange={onChangeHandler}
-                                                                value="yes"
-                                                                checked={values.wifi === "yes"}
-                                                            />
-                                                            Yes
-                                                        </label>
-                                                    </div>
-                                                    <div className="radio">
-                                                        <label>
-                                                            <input
-                                                                type="radio"
-                                                                name="wifi"
-                                                                onChange={onChangeHandler}
-                                                                value="no"
-                                                                checked={values.wifi === "no"}
-                                                            />
-                                                            No
-                                                        </label>
-                                                    </div>
-                                                </div>
+                                                <label htmlFor="select5">WiFi</label>
+                                            </div>
+                                        </div>
+                                        <div className="col-6">
+                                            <div className="form-floating" >
+                                                <select name="parking" value={values.parking} onChange={onChangeHandler} className="form-select" id="select6">
+                                                    <option>Yes</option>
+                                                    <option>No</option>
+                                                </select>
+                                                <label htmlFor="select6">Parking</label>
                                             </div>
                                         </div>
                                         <div className="col-12">
