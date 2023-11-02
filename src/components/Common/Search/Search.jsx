6e7f03cb3867eaa1e307b7dc686f8565c146da70
Daniel Dushev <danieldyshew@gmail.com>
2023-11-02 16:48:53 +0200

    Updated Search input and select fields

--- src/components/Common/Search/Search.jsx
+++ src/components/Common/Search/Search.jsx
@@ -23,58 +23,73 @@ export const Search = () => {
                         <div className="col-md-10">
                             <div className="row g-2">
                                 <div className="col-md-3">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="name"
-                                        placeholder="Property Name"
-                                        name={searchFields.name}
-                                        value={values.name}
-                                        onChange={onChangeHandler}
-                                    />
+                                    <div className="form-floating">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="search-name"
+                                            placeholder="Property Name"
+                                            name={searchFields.name}
+                                            value={values.name}
+                                            onChange={onChangeHandler}
+                                        />
+                                        <label htmlFor="search-name">Property name</label>
+                                    </div>
                                 </div>
                                 <div className="col-md-3">
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        id="price"
-                                        placeholder="Price per night"
-                                        name={searchFields.price}
-                                        value={values.price}
-                                        onChange={onChangeHandler}
-                                    />
+                                    <div className="form-floating">
+                                        <input
+                                            type="number"
+                                            className="form-control"
+                                            id="search-price"
+                                            placeholder="Price per night"
+                                            name={searchFields.price}
+                                            value={values.price}
+                                            onChange={onChangeHandler}
+                                        />
+                                        <label htmlFor="search-price">Price per night</label>
+                                    </div>
                                 </div>
                                 <div className="col-md-3">
-                                    <select name={searchFields.adult}
-                                        value={values.adult}
-                                        onChange={onChangeHandler}
-                                        className="form-select"
-                                    >
-                                        <option disabled>Adults</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                    </select>
+                                    <div className="form-floating">
+                                        <select name={searchFields.adult}
+                                            value={values.adult}
+                                            onChange={onChangeHandler}
+                                            className="form-select"
+                                            id="select7"
+                                        >
+                                            <option disabled>Select</option>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                        <label htmlFor="select7">Adults</label>
+                                    </div>
                                 </div>
                                 <div className="col-md-3">
-                                    <select name={searchFields.child}
-                                        value={values.child}
-                                        onChange={onChangeHandler}
-                                        className="form-select"
-                                    >
-                                        <option disabled>Children</option>
-                                        <option>0</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                    </select>
+                                    <div className="form-floating">
+                                        <select name={searchFields.child}
+                                            value={values.child}
+                                            onChange={onChangeHandler}
+                                            className="form-select"
+                                            id="select8"
+                                        >
+                                            <option disabled>Select</option>
+                                            <option>0</option>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                        <label htmlFor="select8">Children</label>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-2">
-                            <button onClick={onSubmit} className="btn btn-primary w-100">Search</button>
+                            <button onClick={onSubmit} className="btn btn-primary w-100 h-100">Search</button>
                         </div>
                     </div>
                 </div>
