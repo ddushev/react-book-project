90c410f0bc5d22d739e06feceebabf6eba7d3d22
Daniel Dushev <danieldyshew@gmail.com>
2023-11-22 16:56:40 +0200

    Add DailyWeatherCard component for 5 day forecast

---
+++ client/src/components/RoomDetails/WeatherDetails/DailyWeatherCard/DailyWeatherCard.jsx
@@ -0,0 +1,30 @@
+import { fahrenheitToCelsius } from "../../../../utils/convertFahrenheitToCelsius";
+
+export const DailyWeatherCard = ({ index, dailyForecast }) => {
+    return (
+        <div className="d-flex mb-3">
+            <small className="border-end me-3 pe-3">
+                <i className="fas fa-calendar-day text-primary me-2" />
+                Day {index + 1}
+            </small>
+            <small className="border-end me-3 pe-3">
+                <i className="fas fa-temperature-low text-primary me-2" />
+                {fahrenheitToCelsius(dailyForecast?.Temperature?.Minimum?.Value)} &deg;
+            </small>
+            <small className="border-end me-3 pe-3">
+                <i className="fas fa-temperature-high text-primary me-2" />
+                {fahrenheitToCelsius(dailyForecast?.Temperature?.Maximum?.Value)} &deg;
+            </small>
+            <small className="border-end me-3 pe-3">
+                <i className="fas fa-info text-primary me-2" />
+                {dailyForecast?.Day?.IconPhrase}
+            </small>
+            {dailyForecast?.Day?.HasPrecipitation &&
+                <small className="border-end me-3 pe-3">
+                    <i className="fas fa-cloud-rain text-primary me-2" />
+                    {`${dailyForecast?.Day?.PrecipitationIntensity} ${dailyForecast?.Day?.PrecipitationType}`}
+                </small>
+            }
+        </div>
+    );
+}
