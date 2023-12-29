258e264efae154c1214299cef84cfb5f3a9c4a48
Daniel Dushev <danieldyshew@gmail.com>
2023-12-29 21:17:44 +0200

    keep the ball rolling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,6 +35,7 @@ import { RoomBookedOrConfirmedGuard } from "./components/Common/RoomBookedOrConf
 
 
 function App() {
+    // keep the ball rolling
     return (
         <div className="container-xxl bg-white p-0">
             <AuthContextProvider>
